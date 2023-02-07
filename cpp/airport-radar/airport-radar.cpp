8379c6a2b274c39b6ff56258fe397ff60343b33c
parkerconley <parker.r2003@gmail.com>
2023-02-06 19:04:44 -0700

    practicing c++ functions

---
+++ cpp/airport-radar/airport-radar.cpp
@@ -0,0 +1,28 @@
+// Parker Conley
+// 02.06.23
+// This program simulates an airport radar installation.
+
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+// Function declarations
+
+void CalcDist(int x1, int x2, int y1, int y2);
+
+void ConvertRectToPolar(int x, int y, int &distance, int &angle);
+void ConvertPolarToRect(int distance, int angle, int &x, int &y);
+
+void GetRectCoords(int &x, int &y);
+
+int main() {
+
+
+
+    return 0;
+}
+
+void CalcDist(int x1, int x2, int y1, int y2) {
+    int distance{};
+
+}
