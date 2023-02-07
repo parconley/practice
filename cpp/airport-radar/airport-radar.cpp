38f1897678e1baa0b96b55f43639c3045c86f118
parconl <65985086+parconl@users.noreply.github.com>
2023-02-07 16:11:33 -0700

    designing program

--- cpp/airport-radar/airport-radar.cpp
+++ cpp/airport-radar/airport-radar.cpp
@@ -6,12 +6,30 @@
 using namespace std;
 #include <math.h>
 
-// Function declarations
+// Function declartions
 
+// Customer Service Representatives
+// Phase 1
+
+// Phase 2
+
+
+// Inputs
+// Phase 1
+
+
+// Phase 2
+
+
+// Engineers
+// Phase 1
 void CalcDist(int x1, int x2, int y1, int y2);
+void RectToPolar(int x, int y, int &distance, int &angle);
+void PolarToRect(int distance, int angle, int &x, int &y);
 
-void ConvertRectToPolar(int x, int y, int &distance, int &angle);
-void ConvertPolarToRect(int distance, int angle, int &x, int &y);
+//Phase 2
+
+
 
 void GetRectCoords(int &x, int &y);
 
