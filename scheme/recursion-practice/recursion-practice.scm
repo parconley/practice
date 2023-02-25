0415794b9c8337b3891746a51a9ff5a76aa0227e
parconl <parker.r2003@gmail.com>
2023-02-25 13:30:34 -0700

    practicing recursion in scheme

---
+++ scheme/recursion-practice/recursion-practice.scm
@@ -0,0 +1,19 @@
+; Name: Parker
+; Date: 02.22.23
+; Description: This program has two funtions that use recursion.
+; The first function returns the nth term in a Geometric Sequence.
+; The second function returns the value of a base raised to an exponent.
+
+#lang scheme
+
+(define (GeometricSequence scale_factor ratio n)
+  (if (= n 1)
+      scale_factor
+      (* ratio (GeometricSequence scale_factor ratio (- n 1)))))
+
+(define (PowerFunction base exponent)
+  (if (= exponent 0)
+    1
+    (if (even? exponent)
+        (expt (PowerFunction base (/ exponent 2)) 2)
+        (* base (expt (PowerFunction base (/ (- exponent 1) 2)) 2)))))
