3a13e3059113f1871733d490d8dd859a1e13e3b4
parkerconley <parker.r2003@gmail.com>
2023-03-01 18:40:08 -0700

    messing around

--- random/random.cpp
+++ random/random.cpp
@@ -6,17 +6,7 @@ using namespace std;
 void intcopy(int dest[], int source[], int size);
 
 int main() {
-    int array1[5] = { 1, 2, 3, 4, 5 };
-    //int array2[5] = { 6, 7, 8, 9, 10 };
-    int array3[5];
-
-    intcopy(array3, array1, 5);
-
-    for (int i = 0; i < 5; i++) {
-        cout << array3[i] << endl;
-    }
-
-    return 0;
+    
 }
 
 void intcopy(int dest[], int source[], int size) {
---
+++ random/random.scm
@@ -0,0 +1,10 @@
+#lang scheme
+(define List '(1 2))
+
+List
+
+(cons 1 (cons 1 2))
+
+(cons 1 `())
+
+(cons 1 (cons 2 `()))
