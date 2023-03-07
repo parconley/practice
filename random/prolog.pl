de7efc499f44aad2a75b6a90fa1ab9f9a13a68a0
parkerconley <parker.r2003@gmail.com>
2023-03-06 19:06:10 -0700

    playing with prolog

---
+++ random/prolog.pl
@@ -0,0 +1,8 @@
+% Name: Parker Conley
+% Date: 03.06.2023
+% Description: Messing around with prolog
+
+fullname(fred, flintstone).
+fullname(barney, rubble).
+fullname(joe, rockhead).
+fullname(cyrus, slate).
