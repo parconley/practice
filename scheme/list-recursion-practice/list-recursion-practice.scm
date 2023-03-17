1029c4bff3b47c769b71ca314d8c3bfdc9a51e5b
parconl <parker.r2003@gmail.com>
2023-03-17 14:59:30 -0700

    working on list-recursion-practice.scm

--- scheme/list-recursion-practice/list-recursion-practice.scm
+++ scheme/list-recursion-practice/list-recursion-practice.scm
@@ -4,18 +4,47 @@
 
 #lang scheme
 
+; Problem (a)
+
 ; This function takes two vectors in the form of lists
 ; and returns the dot product.
 (define (dotProduct vector1 vector2)
   (if (null? vector1)
       0
-      (+ (* (car vector1) (car vector2)) (DotProduct (cdr vector1) (cdr vector2)))))
+      (+ (* (car vector1) (car vector2)) (dotProduct (cdr vector1) (cdr vector2)))))
 
-(define (duplicates? list1 list2)
-  (if (null? list1)
+; Problem (b)
+
+; This function takes a list.
+; It returns #t if the list has duplicate atoms.
+; It returns #f if the list does not have duplicate atoms.
+(define (duplicates? list)
+  (if (null? list)
+      #f
+      (if (myMember (cdr list) (car list))
+          #t
+          (duplicates? (cdr list)))))
+
+; This function takes a list and an atom.
+; It returns #t if the atom is in the list.
+; It returns #f if the atom is not in the list.
+(define (myMember list atom)
+    (if (null? list)
+         #f
+         (if (= (car list) atom)
+              #t
+              (myMember (cdr list) atom))))
+
+; Problem (c)
+
+(define (distance pairs)
+  (if (null? (cdr pairs))
       0
-      (if (null? list2)
-          0
-          (if (eq? (car list2) (car list2))
-              (+ 1 (duplicates? (cdr list1) (cdr list2)))))))
-; this code is incorrect where it's at. It needs to cycle through one at a time, not both at a time.
+      (+ (sqrt (+ (expt (- (car (cdr pairs)) (car pairs)) 2)
+                  (expt (- (car (cdr pairs)) (car pairs)) 2)))
+         (distance (cdr pairs)))))
+
+; I did not impliment a way to nagivate with two lists. Fix this next time around
+    
+
+         
