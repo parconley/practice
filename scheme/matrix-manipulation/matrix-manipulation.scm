1919769995fc96721b7fa90b6a343280a23acc8a
parconl <parker.r2003@gmail.com>
2023-03-24 14:29:22 -0700

    added funtions to matrix-manipulation.scm

--- scheme/matrix-manipulation/matrix-manipulation.scm
+++ scheme/matrix-manipulation/matrix-manipulation.scm
@@ -7,14 +7,15 @@
 
 #lang scheme
 
+; This function returns the cell at the specified row and column of the matrix.
 (define (getCell Matrix Row Column)
   (if (null? Matrix)
       #f
       (if (= Row 1)
           (getCellInRow (car Matrix) Column)
-           (getCell (cdr Matrix) (- Row 1) Column))))
+          (getCell (cdr Matrix) (- Row 1) Column))))
 
-
+; This function is called by "getCell" to get a cell in a row of a matrix.
 (define (getCellInRow Matrix Column)
   (if (null? Matrix)
       #f
@@ -22,5 +23,19 @@
           (car Matrix)
           (getCellInRow (cdr Matrix) (- Column 1)))))
 
-;(define (setCell Matrix Row Column)
-;  ())
+; This function sets the cell at the specified row and column of the matrix to
+; the specified item.
+(define (setCell Matrix Row Column Item)
+  (if (null? Matrix)
+      '()
+      (if (= Row 1)
+          (cons (setCellInRow (car Matrix) Column Item) (setCell (cdr Matrix) (- Row 1) Column Item))
+          (cons (car Matrix) (setCell (cdr Matrix) (- Row 1) Column Item)))))
+
+; This function is called by "setCell" to set a cell in a row of a matrix.
+(define (setCellInRow Matrix Column Item)
+  (if (null? Matrix)
+      '()
+      (if (= Column 1)
+          (cons Item (setCellInRow (cdr Matrix) (- Column 1) Item))
+          (cons (car Matrix) (setCellInRow (cdr Matrix) (- Column 1) Item)))))
