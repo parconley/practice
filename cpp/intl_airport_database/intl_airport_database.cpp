6a1754cc189b1208a5e97e3124831dbbdcf4c364
parconl <parker.r2003@gmail.com>
2023-05-05 14:43:52 -0700

    modified 3 files

--- cpp/intl_airport_database/intl_airport_database.cpp
+++ cpp/intl_airport_database/intl_airport_database.cpp
@@ -19,9 +19,9 @@ struct Airport {
     double _long; // Longitude
 };
 
-void loadAirports(Airport airports[], int &nAirports);
-void sortAirports(Airport airports[], int nAirports);
-Airport searchAirports(Airport airports[], int nAirports, char key[]);
+void loadAirports(Airport *airports[], int &nAirports);
+void sortAirports(Airport *airports[], int nAirports);
+Airport* searchAirports(Airport *airports[], int nAirports, char key[]);
 int str_compare(char s1[], char s2[]);
 void str_copy(char dest[], char src[]);
 
@@ -32,7 +32,7 @@ int main() {
     cout << "using the airport code." << endl << endl;
 
     // Load data into array
-    Airport airports[100];
+    Airport *airports[100];
     int nAirports = 0;
     loadAirports(airports, nAirports);
 
@@ -47,20 +47,20 @@ int main() {
         cout << "Please enter an airport code: ";
         cin >> key;
 
-        Airport foundAirport = searchAirports(airports, nAirports, key);
+        Airport *foundAirport = searchAirports(airports, nAirports, key);
         char notFoundOrig[4] = "NOT";
 
-        if (str_compare(foundAirport._orig, notFoundOrig) == 0) {
+        if (str_compare(foundAirport->_orig, notFoundOrig) == 0) {
             cout << endl;
             cout << "Airport not found." << endl << endl;
         } else {
             cout << endl;
             cout << "Airport found!" << endl << endl;
-            cout << "Airport name: " << foundAirport._name << endl;
-            cout << "Total seats: " << foundAirport._totalSeats << endl;
-            cout << "Country: " << foundAirport._country << endl;
-            cout << "Latitude: " << foundAirport._lat << endl;
-            cout << "Longitude: " << foundAirport._long << endl << endl;
+            cout << "Airport name: " << foundAirport->_name << endl;
+            cout << "Total seats: " << foundAirport->_totalSeats << endl;
+            cout << "Country: " << foundAirport->_country << endl;
+            cout << "Latitude: " << foundAirport->_lat << endl;
+            cout << "Longitude: " << foundAirport->_long << endl << endl;
         }
 
         cout << "Would you like to search for another airport? (1 = yes, 0 = no): ";
@@ -70,42 +70,46 @@ int main() {
 }
 
 // Load data into array
-void loadAirports(Airport airports[], int &nAirports) {
+void loadAirports(Airport *airports[], int &nAirports) {
     fstream infile;
 
     infile.open("airports.txt", ios::in);
 
     nAirports = 0;
     char end[4] = "END";
+    Airport temp;
 
-    infile >> airports[nAirports]._orig;
-    while (str_compare(airports[nAirports]._orig, end) != 0) {
-        infile >> airports[nAirports]._name;
-        infile >> airports[nAirports]._totalSeats;
-        infile >> airports[nAirports]._country;
-        infile >> airports[nAirports]._lat;
-        infile >> airports[nAirports]._long;
-
+    infile >> temp._orig;
+    while (str_compare(temp._orig, end) != 0) {
+        Airport *newAirport = new Airport;
+        str_copy(newAirport->_orig, temp._orig);
+        infile >> newAirport->_name;
+        infile >> newAirport->_totalSeats;
+        infile >> newAirport->_country;
+        infile >> newAirport->_lat;
+        infile >> newAirport->_long;
+        airports[nAirports] = newAirport;
         nAirports++;
 
-        infile >> airports[nAirports]._orig;
+        infile >> temp._orig;
     }
 
     infile.close();
 }
 
 // Selection sort
-void sortAirports(Airport airports[], int nAirports) {
+void sortAirports(Airport *airports[], int nAirports) {
     int i, j, minIndex;
-    Airport temp;
+    Airport *temp;
 
     for (i = 0; i < nAirports - 1; i++) {
         minIndex = i;
         for (j = i + 1; j < nAirports; j++) {
-            if (str_compare(airports[j]._orig, airports[minIndex]._orig) < 0) {
+            if (str_compare(airports[j]->_orig, airports[minIndex]->_orig) < 0) {
                 minIndex = j;
             }
         }
+
         temp = airports[i];
         airports[i] = airports[minIndex];
         airports[minIndex] = temp;
@@ -113,44 +117,48 @@ void sortAirports(Airport airports[], int nAirports) {
 }
 
 // Binary search
-Airport searchAirports(Airport airports[], int nAirports, char key[]) {
+Airport* searchAirports(Airport *airports[], int nAirports, char key[]) {
     int low = 0;
     int high = nAirports - 1;
     int mid;
 
     while (low <= high) {
         mid = (low + high) / 2;
-        if (str_compare(airports[mid]._orig, key) < 0) {
+        if (str_compare(airports[mid]->_orig, key) < 0) {
             low = mid + 1;
-        } else if (str_compare(airports[mid]._orig, key) > 0) {
+        } else if (str_compare(airports[mid]->_orig, key) > 0) {
             high = mid - 1;
         } else {
             return airports[mid];
         }
     }
 
-    Airport notFound;
+    Airport *notFound = new Airport;
     char notFoundOrig[4] = "NOT";
 
-    str_copy(notFound._orig, notFoundOrig);
+    str_copy(notFound->_orig, notFoundOrig);
     return notFound;
 }
 
 // Compare two strings, return 0 if equal, < 0 if s1 < s2, > 0 if s1 > s2
 int str_compare(char s1[], char s2[]) {
     int i = 0;
+
     while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]) {
         i++;
     }
+    
     return s1[i] - s2[i];
 }
 
 // Copy string s2 to s1
 void str_copy(char dest[], char src[]) {
     int i = 0;
+
     while (src[i] != '\0') {
         dest[i] = src[i];
         i++;
     }
+
     dest[i] = '\0';
 }
--- random/random.cpp
+++ random/random.cpp
@@ -4,11 +4,23 @@
 using namespace std;
 
 int main() {
-    int x{ 1 };
-    int *ptr = &x;
+    int array[5] = {1, 2, 3, 4, 5};
 
-    cout << &ptr << endl;
-    cout << ptr << endl;
-    cout << &x << endl;
-    cout << *ptr << endl;
+    int new_array[5];
+
+    for (int i = 0; i < 5; i++) {
+        new_array[i] = array[i];
+    }
+
+    cout << "Array: " << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << array[i] << endl;
+    }
+
+    cout << "New Array: " << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << new_array[i] << endl;
+    }
+    
+    return 0;
 }
